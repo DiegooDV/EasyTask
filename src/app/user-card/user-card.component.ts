e862f656ee001a462d3df2b38c2fd7eac89a5cfb
Diego De la Vega <>
2024-06-18 17:11:52 -0600

    feat: load random user on user-card

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
+import { DUMMY_USERS } from '../../data/dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+let random = Math.random()
+console.log(random)
+console.log(random * DUMMY_USERS.length)
+console.log(Math.floor(random * DUMMY_USERS.length))
+
 
 @Component({
   selector: 'app-user-card',
@@ -7,5 +16,15 @@ import { Component } from '@angular/core';
   styleUrl: './user-card.component.css'
 })
 export class UserCardComponent {
+  selectedUser = signal(DUMMY_USERS[randomIndex]); 
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  
+  //get imagePath() {
+  //  return 'assets/users/' + this.selectedUser().avatar
+  //}
 
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    this.selectedUser.set(DUMMY_USERS[randomIndex])
+  }
 }
