2dab7e4553bafd87f3707178eb202b27c963cdd0
Diego De la Vega <>
2024-06-19 15:37:26 -0600

    feat: add task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserCardComponent } from './user-card/user-card.component';
 import { DUMMY_USERS } from '../data/dummy-users';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserCardComponent],
+  imports: [HeaderComponent, UserCardComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-angular-app';
   users = DUMMY_USERS;
+  selectedUserId: string = "u1"
+
+  get selectedUser () {
+    return this.users.find(x => x.id === this.selectedUserId)
+  }
 
   onSelectUser(id: string) {
-    console.log(id)
+    this.selectedUserId = id
   }
 }
