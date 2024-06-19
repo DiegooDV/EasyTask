d44734484b783f15746e710d0bcb12dd6d065d2e
Diego De la Vega <>
2024-06-19 16:56:19 -0600

    feat: add @for to list users

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,5 +1,11 @@
 import { Component, EventEmitter, Input, Output} from '@angular/core';
 
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user-card',
   standalone: true,
@@ -7,16 +13,14 @@ import { Component, EventEmitter, Input, Output} from '@angular/core';
   styleUrl: './user-card.component.css'
 })
 export class UserCardComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar
+    return 'assets/users/' + this.user.avatar
   }
 
   onSelectUser() {
-    this.select.emit(this.id)
+    this.select.emit(this.user.id)
   }
 }
