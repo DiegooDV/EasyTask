ee75d3cefcc624c55986278461f1eb8816bfc999
Diego De la Vega <>
2024-06-20 01:38:56 -0600

    feat: add persistance using localStorage

--- src/app/task/task.service.ts
+++ src/app/task/task.service.ts
@@ -1,24 +1,42 @@
-import { Injectable } from "@angular/core";
-import { DUMMY_TASKS } from "../../data/dummy-tasks";
-import { TaskInput } from "./task-input/task-input.model";
+import { Injectable } from '@angular/core';
+import { DUMMY_TASKS } from '../../data/dummy-tasks';
+import { TaskInput } from './task-input/task-input.model';
 
-@Injectable({providedIn: "root"})
+@Injectable({ providedIn: 'root' })
 export class TaskService {
-    private tasks = DUMMY_TASKS;
+  private readonly LOCAL_STORAGE_TASKS_KEY = 'tasks';
+  private tasks = DUMMY_TASKS;
 
-    getUserTasks(userId: string) {
-        return this.tasks.filter((task) => task.userId === userId);
+  constructor() {
+    const tasks = localStorage.getItem(this.LOCAL_STORAGE_TASKS_KEY);
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
     }
+  }
 
-    addTask(taskInput: TaskInput, userId: string) {
+  getUserTasks(userId: string) {
+    return this.tasks.filter((task) => task.userId === userId);
+  }
+
+  addTask(taskInput: TaskInput, userId: string) {
     this.tasks.push({
-        ...taskInput,
-        userId: userId,
-        id: new Date().getMilliseconds().toString(),
-        });
-    }
+      ...taskInput,
+      userId: userId,
+      id: new Date().getMilliseconds().toString(),
+    });
+    this.saveTasks();
+  }
 
-    removeTask(taskId: string){
-        this.tasks = this.tasks.filter((task) => taskId !== task.id);
-    }
+  removeTask(taskId: string) {
+    this.tasks = this.tasks.filter((task) => taskId !== task.id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem(
+      this.LOCAL_STORAGE_TASKS_KEY,
+      JSON.stringify(this.tasks)
+    );
+  }
 }
